b2b751034a5bd8aa1761d5b59478f0b9c1a08e08
marty90 <martino.trevisan@polito.it>
2017-11-28 13:35:53 +0100

    improved maintenance

--- run_query.py
+++ run_query.py
@@ -9,7 +9,7 @@ import importlib
 import json
 import os.path
 import zipfile
-
+import core.utils
 
 def main():
 
@@ -19,27 +19,21 @@ def main():
     parser = argparse.ArgumentParser(description='NetLytics Job')
 
     parser.add_argument('--input_path', metavar='input_path', type=str, 
-                       help='Base Log Files Input Path')   
-                       
+                       help='Base Log Files Input Path')                          
     parser.add_argument('--output_file_local', metavar='output_file_local', type=str, default= "",
-                       help='File where the resulting table is saved (locally). Cannot be specified together with output_file_HDFS')
-
+                       help='File where the resulting table is saved (locally). \
+                             Cannot be specified together with output_file_HDFS')
     parser.add_argument('--output_file_HDFS', metavar='output_file_HDFS', type=str, default= "",
-                       help='File where the resulting table is saved (HDFS).  Cannot be specified together with output_file_local')
-                       
+                       help='File where the resulting table is saved (HDFS). \
+                             Cannot be specified together with output_file_local')                       
     parser.add_argument('--query', metavar='query', type=str, 
                        help='SQL Query to exectute. Use "netlytics" as SQL table name') 
-
     parser.add_argument('--connector', metavar='connector', type=str, 
                         help='Connector class name')  
-
     parser.add_argument('--start_day', metavar='start_day', type=str, 
                         help='Start day for analysis, format YYYY_MM_DD')
-
     parser.add_argument('--end_day', metavar='end_day', type=str, 
                         help='End day for analysis, format YYYY_MM_DD')
-
-    
 
     # Get parameters
     args = vars(parser.parse_args())
@@ -70,32 +64,8 @@ def main():
     sc = SparkContext(conf = conf)
     spark = SparkSession(sc)
 
-    # Ship code to executors
-    ship_dir (base_path + "/algos",sc, base_path)
-    ship_dir (base_path + "/core",sc, base_path)
-    ship_dir (base_path + "/connectors",sc, base_path)
-
-    # Find connector
-    connector_module = my_import(connector,sc)
-
-    # Parse dates
-    y1,m1,d1 = start_day.split("_")
-    date1 = date (int(y1),int(m1),int(d1))
-    y2,m2,d2 = end_day.split("_")
-    date2 = date (int(y2),int(m2),int(d2))
-
-    # Instantiate connector
-    connector_instance = connector_module(input_path,date1,date2 )
-    
-    # Get and enforce Schema
-    output_type = connector_instance.output_type
-    schema_file = base_path + "/schema/" + output_type + ".json"
-    schema_json = json.load(open(schema_file,"r"))
-    schema = StructType.fromJson(schema_json)
-    connector_instance.set_schema (schema)
-
-    # Get Dataset
-    dataset = connector_instance.get_DF(sc,spark)
+    # Create the dataframe
+    dataset = core.utils.get_dataset(sc,spark,base_path,connector,input_path,start_day, end_day )
     dataset.createOrReplaceTempView("netlytics")
 
     # Execute Query
@@ -119,20 +89,6 @@ def my_import(name,sc):
     my_class = getattr(module, labels[-1])
     return my_class
 
-n=0
-def ship_dir(path,sc, base_path):
-    global n
-    n+=1
-    zipf = zipfile.ZipFile('/tmp/' + str(n)+ '.zip', 'w', zipfile.ZIP_DEFLATED)
-    zipdir(path + '/', zipf, base_path)
-    zipf.close()
-    sc.addPyFile('/tmp/' + str(n)+ '.zip')
-
-def zipdir(path, ziph, base_path):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file),  os.path.relpath(os.path.join(root, file),base_path )  )
 
 main()
 
